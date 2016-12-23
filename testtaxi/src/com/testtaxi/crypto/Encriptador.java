bda143fb0ba8f996f7475a8da3e945caa4a739a6
Javier Martín Alonso <xavimartin@hotmail.com>
2016-12-23 11:47:42 +0100

    first commict

--- testtaxi/src/com/testtaxi/crypto/Encriptador.java
+++ testtaxi/src/com/testtaxi/crypto/Encriptador.java
@@ -1,19 +1,17 @@
 package com.testtaxi.crypto;
 
-import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 
-import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.DESKeySpec;
 
 import org.apache.log4j.Logger;
+
+import com.sun.xml.ws.util.UtilException;
 
 //@SuppressWarnings("restriction")
 public final class Encriptador 
@@ -24,28 +22,42 @@ public final class Encriptador
 	//SEGUIMIENTO LOG
 	private static Logger log = Logger.getLogger(Encriptador.class);
 
-	private Encriptador()
+	private Encriptador() throws UtilException 
 	{			
 		String chiave = "KeyPassw0rd";
 
 		java.security.Security.addProvider(new com.sun.crypto.provider.SunJCE());
 
-
-			try {
-				DESKeySpec desKeySpec = new DESKeySpec(chiave.getBytes());
-				SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
-				SecretKey key = keyFactory.generateSecret(desKeySpec);
-				ecipher = Cipher.getInstance("DES");
-				dcipher = Cipher.getInstance("DES");
-				ecipher.init(Cipher.ENCRYPT_MODE, key);
-				dcipher.init(Cipher.DECRYPT_MODE, key);
-			} 
-			catch (Exception e) 
-			{
-				// TODO: handle exception
-			}
-
-		
+		try 
+		{
+			DESKeySpec desKeySpec = new DESKeySpec(chiave.getBytes());
+			SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+			SecretKey key = keyFactory.generateSecret(desKeySpec);
+			ecipher = Cipher.getInstance("DES");
+			dcipher = Cipher.getInstance("DES");
+			ecipher.init(Cipher.ENCRYPT_MODE, key);
+			dcipher.init(Cipher.DECRYPT_MODE, key);
+		} 
+		catch (javax.crypto.NoSuchPaddingException e) 
+		{
+			log.error("ERROR : Se ha producido un error",e);			
+			throw new UtilException("ERROR : Se ha producido un error",Encriptador.class,"Encriptador",e);			
+		} 
+		catch (java.security.NoSuchAlgorithmException e) 
+		{
+			log.error("ERROR : Se ha producido un error",e);			
+			throw new UtilException("ERROR : Se ha producido un error",Encriptador.class,"Encriptador",e);			
+		} 
+		catch (java.security.InvalidKeyException e) 
+		{
+			log.error("ERROR : Se ha producido un error",e);			
+			throw new UtilException("ERROR : Se ha producido un error",Encriptador.class,"Encriptador",e);			
+		} 
+		catch (InvalidKeySpecException e) 
+		{
+			log.error("ERROR : Se ha producido un error",e);			
+			throw new UtilException("ERROR : Se ha producido un error",Encriptador.class,"Encriptador",e);
+		}
 	}
 
 	
@@ -55,22 +67,34 @@ public final class Encriptador
 	 * @return cadena encriptada
 	 * @throws UtilException 
 	 */
-	private String encrypt(String str)
+	private String encrypt(String str) throws UtilException 
 	{
+		try 
+		{
+			// Encode the string into bytes using utf-8
+			byte[] utf8 = str.getBytes("UTF8");
 
-			try {
-				// Encode the string into bytes using utf-8
-				byte[] utf8 = str.getBytes("UTF8");
-				// Encrypt
-				byte[] enc = ecipher.doFinal(utf8);
-				// Encode bytes to base64 to get a string
-				return new sun.misc.BASE64Encoder().encode(enc);
-			} catch (Exception e) {
-				// TODO: handle exception
-			}
-			return str;
+			// Encrypt
+			byte[] enc = ecipher.doFinal(utf8);
 
-		
+			// Encode bytes to base64 to get a string
+			return new sun.misc.BASE64Encoder().encode(enc);
+		} 
+		catch (javax.crypto.BadPaddingException e) 
+		{
+			log.error("ERROR : Se ha producido un error",e);			
+			throw new UtilException("ERROR : Se ha producido un error",Encriptador.class,"encrypt(String str)",e);
+		} 
+		catch (IllegalBlockSizeException e) 
+		{
+			log.error("ERROR : Se ha producido un error",e);			
+			throw new UtilException("ERROR : Se ha producido un error",Encriptador.class,"encrypt(String str)",e);			
+		} 
+		catch (UnsupportedEncodingException e) 
+		{
+			log.error("ERROR : Se ha producido un error",e);			
+			throw new UtilException("ERROR : Se ha producido un error",Encriptador.class,"encrypt(String str)",e);			
+		}
 	}
 	
 	
@@ -80,33 +104,39 @@ public final class Encriptador
 	 * @return cadena desencriptada
 	 * @throws UtilException 
 	 */
-	private String decrypt(String cadena)
+	private String decrypt(String cadena) throws UtilException 
 	{
+		try 
+		{
+			// Decode base64 to get bytes
+			byte[] dec = new sun.misc.BASE64Decoder().decodeBuffer(cadena);
 
-			try {
-				// Decode base64 to get bytes
-				byte[] dec = new sun.misc.BASE64Decoder().decodeBuffer(cadena);
+			// Decrypt
+			byte[] utf8 = dcipher.doFinal(dec);
 
-				// Decrypt
-				byte[] utf8 = dcipher.doFinal(dec);
-
-				// Decode using utf-8
-				return new String(utf8, "UTF8");
-			} catch (IllegalBlockSizeException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (BadPaddingException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (UnsupportedEncodingException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			return cadena;
-		
+			// Decode using utf-8
+			return new String(utf8, "UTF8");
+		}
+		catch (javax.crypto.BadPaddingException e) 
+		{
+			log.error("ERROR : Se ha producido un error desencriptando la cadena: '"+cadena+ "'.\nASEGURATE QUE LA CADENA EST� ENCRIPTADA.",e);			
+			throw new UtilException("ERROR : Se ha producido un error desencriptando la cadena: '"+cadena+ "'.\nASEGURATE QUE LA CADENA EST� ENCRIPTADA.",Encriptador.class,"decrypt(String cadena)",e);			
+		}
+		catch (IllegalBlockSizeException e) 
+		{
+			log.error("ERROR : Se ha producido un error desencriptando la cadena: '"+cadena+ "'.\nASEGURATE QUE LA CADENA EST� ENCRIPTADA.",e);			
+			throw new UtilException("ERROR : Se ha producido un error desencriptando la cadena: '"+cadena+ "'.\nASEGURATE QUE LA CADENA EST� ENCRIPTADA.",Encriptador.class,"decrypt(String cadena)",e);			
+		} 
+		catch (UnsupportedEncodingException e) 
+		{
+			log.error("ERROR : Se ha producido un error desencriptando la cadena: '"+cadena+ "'.\nASEGURATE QUE LA CADENA EST� ENCRIPTADA.",e);			
+			throw new UtilException("ERROR : Se ha producido un error desencriptando la cadena: '"+cadena+ "'.\nASEGURATE QUE LA CADENA EST� ENCRIPTADA.",Encriptador.class,"decrypt(String cadena)",e);						
+		} 
+		catch (java.io.IOException e) 
+		{
+			log.error("ERROR : Se ha producido un error desencriptando la cadena: '"+cadena+ "'.\nASEGURATE QUE LA CADENA EST� ENCRIPTADA.",e);			
+			throw new UtilException("ERROR : Se ha producido un error desencriptando la cadena: '"+cadena+ "'.\nASEGURATE QUE LA CADENA EST� ENCRIPTADA.",Encriptador.class,"decrypt(String cadena)",e);						
+		}
 	}
 	
 	
@@ -114,16 +144,22 @@ public final class Encriptador
 	 * Metodo para encriptar cadenas
 	 * @throws UtilException 
 	 */
-	public static String encriptar(String cadena)
+	public static String encriptar(String cadena) throws UtilException 
 	{
 		String encrypted = "";
 
-
+		try 
+		{
 			java.security.Security.addProvider(new com.sun.crypto.provider.SunJCE());
 
 			Encriptador encrypter = new Encriptador();
 			encrypted = encrypter.encrypt(cadena);
-
+		} 
+		catch (Exception e) 
+		{
+			log.error("ERROR : Se ha producido un error encriptando la cadena: '"+cadena+ "'",e);			
+			throw new UtilException("ERROR : Se ha producido un error encriptando la cadena: '"+cadena+ "'",Encriptador.class,"encriptar(String cadena)",e);			
+		}
 		
 		return encrypted;		
 	}
@@ -133,7 +169,7 @@ public final class Encriptador
 	 * Metodo para desencriptar cadenas 
 	 * @throws UtilException 
 	 */
-	public static String desencriptar(String cadena)
+	public static String desencriptar(String cadena) throws UtilException
 	{
 		/*con el metodo anterior es identico al de cifrar, solo que en
 		 * el momento de descifrar solicitamos el numero de espacios que la cadena habia sido dezplazada
@@ -141,12 +177,18 @@ public final class Encriptador
 		 */
 		String decrypted="";
 
-
+		try 
+		{
 			java.security.Security.addProvider(new com.sun.crypto.provider.SunJCE());
 
 			Encriptador encrypter = new Encriptador();
 			decrypted = encrypter.decrypt(cadena);
-
+		}
+		catch (Exception e) 
+		{
+			log.error("ERROR : Se ha producido un error desencriptando la cadena: '"+cadena+ "'.\nASEGURATE QUE LA CADENA EST� ENCRIPTADA.",e);			
+			throw new UtilException("ERROR : Se ha producido un error desencriptando la cadena: '"+cadena+ "'.\nASEGURATE QUE LA CADENA EST� ENCRIPTADA.",Encriptador.class,"desencriptar(String cadena)",e);			
+		}
 		
 		return decrypted;
 	}
